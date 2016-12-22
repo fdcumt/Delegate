579d10357d4d78e12fba195045dd87f0cc8229c1
fdcumt <fdcumt@163.com>
2016-12-22 23:41:22 +0800

    绑定类成员函数, ok

--- Delegate/main.cpp
+++ Delegate/main.cpp
@@ -3,25 +3,25 @@
 using namespace std;
 
 
-template<typename _ClassName, typename _Param1>
+template<class _ClassName, class _Param1>
 class FuncOfMem
 {
 public:
-	typedef void (_ClassName::*PMemFuncType)(_Param1 param1);
+	typedef void (_ClassName::*PtrMemFuncType)(_Param1 param1);
 
-	PMemFuncType m_pFuncMem;
+	PtrMemFuncType m_ptrMemFuncType;
 	_ClassName *m_obj;
 	_Param1 param1;
 
-	void register(typename _ClassName* pObj, PMemFuncType pFunc)
+	void mytest(_ClassName *pObj, PtrMemFuncType pFunc)
 	{
 		m_obj = pObj;
-		m_pFuncMem = pFunc;
+		m_ptrMemFuncType = pFunc;
 	}
 
 	void invoke(_Param1 param1)
 	{
-		pObj->(*pFunc)(param1);
+		(m_obj->*m_ptrMemFuncType)(param1);
 	}
 
 };
@@ -30,26 +30,21 @@ public:
 class Test
 {
 public:
-	void funcA(int a) { cout <<  " funcA::::" << a<<":::end"<< endl; }
+	int mmm;
+	void funcA(int a) { cout << mmm<< " funcA::::" << a<<":::end"<< endl; }
 
 };
 
 int main()
 {
-	cout << " hello world !!!" << endl;
 
 	Test test1;
 	FuncOfMem<Test, int> funcOfMem;
-	funcOfMem.register(&test1, &test1::funcA);
+	funcOfMem.mytest(&test1, &Test::funcA);
+	test1.mmm = 10;
+
 
 	funcOfMem.invoke(3);
-
-#ifdef 1
-
-
-
-
-#endif // 1
 
 
 
