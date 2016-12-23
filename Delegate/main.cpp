8e00970605844d6e52cad084bae74acff8c1dadd
ohcumt <1040183251@qq.com>
2016-12-23 15:28:54 +0800

    未整理版, c++实现委托(使用模板实现)

--- Delegate/main.cpp
+++ Delegate/main.cpp
@@ -2,76 +2,122 @@
 #include <vector>
 using namespace std;
 
-
-template<class _ClassName, class _Param1>
-class FuncOfMem
+template< class _Param >
+class BaseFuncOfMem
 {
+public:
+	virtual void invoke(_Param ) = 0;
+};
+
+
+template< class _ClassName, class _Param>
+class FuncOfMem : public BaseFuncOfMem<_Param>
+{
+public:
+	typedef void (_ClassName::*PtrFuncOfMem)(_Param);
 
 public:
-	FuncOfMem()
+	FuncOfMem(_ClassName *InObj, PtrFuncOfMem InPtrFuncOfMem)
+	{
+		m_obj = InObj;
+		m_ptrFuncOfMem = InPtrFuncOfMem;
+	}
+
+	virtual void invoke(_Param InParam) override
+	{
+		if (m_obj && m_ptrFuncOfMem)
+		{
+			(m_obj->*m_ptrFuncOfMem)(InParam);
+		}
+	}
+
+private:
+	_ClassName *m_obj;
+	PtrFuncOfMem m_ptrFuncOfMem;
+};
+
+template<class _Param1>
+class FuncOfMemManager
+{
+public:
+	FuncOfMemManager()
 	{
 		m_delegateList.empty();
 	}
 
-public:
-	typedef void (_ClassName::*PtrMemFuncType)(_Param1 param1);
-
-	struct DelegateObj
+	~FuncOfMemManager()
 	{
-		_ClassName *_obj;
-		PtrMemFuncType _ptrMemFuncType;
-	};
-
-	vector<DelegateObj> m_delegateList;
-
-	void dynamic_add(_ClassName *pObj, PtrMemFuncType pFunc)
-	{
-		DelegateObj delegateObj;
-		delegateObj._obj = pObj;
-		delegateObj._ptrMemFuncType = pFunc;
-		m_delegateList.push_back(delegateObj);
-	}
-
-	void invoke(_Param1 param1)
-	{
-		for (int i=0; i<m_delegateList.size(); ++i)
+		for (int i = 0; i < m_delegateList.size(); ++i)
 		{
-			DelegateObj& delegateObj = m_delegateList[i];
-			(delegateObj._obj->*(delegateObj._ptrMemFuncType))(param1);
+			BaseFuncOfMem<_Param1>* ptr = m_delegateList[i];
+			if (ptr)
+			{
+				delete ptr;
+			}
 		}
 	}
 
+public:
+	template< class _ClassName>
+	void AddDynamic(_ClassName *pObj, void (_ClassName::*pFuncOfMem)(_Param1 ))
+	{
+		BaseFuncOfMem<_Param1>* ptr = new FuncOfMem<_ClassName, _Param1>(pObj, pFuncOfMem);
+		m_delegateList.push_back(ptr);
+	}
+
+	void Broadcast(_Param1 param1)
+	{
+		for (int i=0; i<m_delegateList.size(); ++i)
+		{
+			BaseFuncOfMem<_Param1>* ptr = m_delegateList[i];
+			ptr->invoke(param1);
+		}
+	}
+
+private:
+	vector<BaseFuncOfMem<_Param1>*> m_delegateList;
 };
 
-class baseClass
+class MyClass
 {
 public:
-
+	int a;
+	int b;
 };
 
-class Test :public baseClass
+class Test 
 {
 public:
 	int mmm;
-	void funcA(int a) { cout << mmm<< " funcA::::" << a<<":::end"<< endl; }
+	void funcA(MyClass a) { cout << mmm<< " funcA::::" << a.a<<"      "<<a.b<<":::end"<< endl; }
 
 };
 
+class TestA
+{
+public:
+	int mmm;
+	void abc(MyClass a) { cout << mmm << " abc::::"  <<a.a << "      " << a.b<< ":::end" << endl; }
+
+};
+
+
+
 int main()
 {
-
-	typedef void (baseClass::*ffffffff)(int);;
-
 	Test test1;
-	Test test2;
-	FuncOfMem<baseClass, int> funcOfMem;
-	funcOfMem.dynamic_add(&test1, static_cast<ffffffff> (&Test::funcA));
-	funcOfMem.dynamic_add(&test2, static_cast<ffffffff> (&Test::funcA));
+	TestA test2;
+	FuncOfMemManager<MyClass> funcOfMemManager;
+	funcOfMemManager.AddDynamic(&test1, &Test::funcA);
+	funcOfMemManager.AddDynamic(&test2, &TestA::abc);
 	test1.mmm = 10;
-	test2.mmm = 11;
+	test2.mmm = 777;
+	MyClass myClass;
+	myClass.a = 1;
+	myClass.b = 2;
 
 
-	funcOfMem.invoke(3);
+	funcOfMemManager.Broadcast(myClass);
 
 	system("pause");
 	return 0;
